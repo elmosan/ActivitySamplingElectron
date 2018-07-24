7c3a637b043103fcf4bc9e47ac85bfc31886820c
elmosan <33811806+elmosan@users.noreply.github.com>
2018-07-24 15:34:06 +0200

    Style cleanup

--- notifier.js
+++ notifier.js
@@ -1,52 +1,49 @@
-const WindowsBalloon = require('node-notifier').WindowsBalloon;
-// const WindowsToaster = require('node-notifier').WindowsToaster;
-const path  = require('path');
-const $ = require('jquery');
-const script = require('./script.js');
-// const ipcRenderer  = require('electron').ipcRenderer ;
+const WindowsBalloon = require('node-notifier').WindowsBalloon
+// const WindowsToaster = require('node-notifier').WindowsToaster
+const path = require('path')
+const script = require('./script.js')
 
-let notifier;
-// let notifier = new WindowsToaster({});
+let notifier
+// let notifier = new WindowsToaster({})
 
-function doNotify(evt){
-  notifier = new WindowsBalloon({});
+function doNotify(evt) {
+  notifier = new WindowsBalloon({})
 
   notifier.notify({
     title: 'ActivitySampling Current Activity',
-    message: 'What are you doing? \r\rLast activity: ' + script.activity + '\r',
+    message: `What are you doing? \r\rLast activity: ${script.activity}\r`,
     icon: path.join(__dirname, 'Kxmylo-Simple-Utilities-system-monitor.ico'),
     sound: true,
     time: 8000,
     wait: true,
     type: 'info'
   },
-  function(err, response){
+  (err, response) => {
+    throw err
+  })
 
-  });
-
-  notifier.on('click', function(notifierObject, options) {
+  notifier.on('click', (notifierObject, options) => {
     // Triggers if `wait: true` and user clicks notification
-    script.writeLog();
-  });
+    script.createLogEntry()
+  })
 
-  notifier.on('timeout', function(notifierObject, options) {
+  notifier.on('timeout', (notifierObject, options) => {
     // Triggers if `wait: true` and notification closes
-  });
+  })
 }
 
 // ipcRenderer.on('timer-message', (event, arg) => {
-//   doNotify();
-// });
+//   doNotify()
+// })
 
-// document.addEventListener('DOMContentLoaded', function() {
-  // document.getElementById("btnStart").addEventListener("click", doNotify);
-// });
+// document.addEventListener('DOMContentLoaded', () => {
+//   document.getElementById("btnStart").addEventListener("click", doNotify)
+// })
 
-setInterval(function() {
-  console.log("Timer intervall", new Date());
-  // ipcRenderer.send('timer-message', 'tick');
+setInterval(() => {
+  console.log('Timer intervall', new Date())
+  // ipcRenderer.send('timer-message', 'tick')
 
-  let args;
-  doNotify(args);
-
-}, 900000);
+  let args
+  doNotify(args)
+}, 900000)
